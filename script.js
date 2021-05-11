67960a1bf7c92beebea8808ba64a331228f84f7c
Mateus Santos <mateus.sa94@hotmail.com>
2021-05-11 20:32:58 -0300

    modifiquei os arquivos index.html e script.js

--- script.js
+++ script.js
@@ -43,9 +43,39 @@ let currentItem = 0;
 
 //load initial item
 window.addEventListener("DOMContentLoaded",function(){
-	const item =review[currentItem];
+	const item =reviews[currentItem];
 	img.src = item.img;
 	author.textContent = item.name;
 	job.textContent = item.job;
 	info.textContent = item.text;
+});
+
+//show person based on item
+function showPerson(person){
+	const item = reviews[person];
+	img.src = item.img;
+	author.textContent = item.name;
+	job.textContent = item.job;
+	info.textContent = item.text;
+}
+//show next person
+nextBtn.addEventListener("click",function(){
+	currentItem++;
+	if(currentItem > reviews.length - 1){
+		currentItem = 0;
+	}
+	showPerson(currentItem);
+});
+//show prev person
+prevBtn.addEventListener("click",function(){
+	currentItem--;
+	if(currentItem < 0){
+		currentItem = reviews.length - 1;
+	}
+	showPerson(currentItem);
+});
+//show random person
+random.addEventListener("click",function(){
+	currentItem = Math.floor(Math.random() * reviews.length);
+	showPerson(currentItem);
 });
